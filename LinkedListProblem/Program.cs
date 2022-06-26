fe2085f4fdfb2cbdadba2b945b69712c6f603ca5
Prateek Pai <paiprateek123@gmail.com>
2022-06-26 10:42:08 +0530

    Craete a Linked list of 56, 30 and 70

--- LinkedListProblem/Program.cs
+++ LinkedListProblem/Program.cs
@@ -2,11 +2,81 @@
 
 namespace LinkedListProblem
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Linked List Problem");
+            Console.WriteLine("Creating Linked lIst");
+            Console.WriteLine("\n");
+
+            Program program = new Program();
+            Console.WriteLine("Creating  Linked list by adding Data in the First Position ");
+            program.AddFirst(56);
+            program.AddFirst(30);
+            program.AddFirst(70);
+            program.DiplayData();
+
         }
-    }
+
+        // Creating Node
+        public class Node
+        {
+            public int data;
+            public Node next;
+
+            public Node(int data)
+            {
+                this.data = data;
+            }
+        }
+
+
+        public Node head;
+
+        // Creting Method to Add data in First position in the Linked list
+        public bool AddFirst(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+            {
+                head = node;
+                return true;
+            }
+            node.next = head;
+            head = node;
+            return true;
+        }
+
+        // Creating Method to Display the data in Linked lIst
+        public void DiplayData()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("Linkedlist is Empty");
+            }
+            Node temp = head;
+            while (temp != null)
+            {
+                Console.Write(temp.data + " " );
+                temp = temp.next;
+            }
+        }
+        // Creating method to add data in Last postion in the Linked list
+        public bool AddLast(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+            {
+                head = node;
+                return true;
+            }
+            Node temp = head;
+            while (temp.next != null)
+            {
+                temp = temp.next;
+            }
+            temp.next = node;
+            return true;
+        }
+    }   
 }
